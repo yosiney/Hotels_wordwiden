d5e30d76332240f4ee9cd718b108125c33f326b8
yosin <yosin@DESKTOP-VTB1K29>
2024-02-12 21:50:56 -0600

    first update

---
+++ hotels_worldwiden/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotels_worldwiden
+{
+    internal class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            //Yosiney: DESKTOP-VTB1K29\SQLEXPRESS//
+            SqlConnection conectar = new SqlConnection("Server=DESKTOP-VTB1K29\\SQLEXPRESS;Database=BDProyectoFinal_progralV;Integrated Security=True;");
+            conectar.Open();
+            return conectar;
+        }
+    }
+}
