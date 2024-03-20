c3ef32a70cfd959ee2d3df6b35c6807a22f4c347
yosin <yosin@DESKTOP-VTB1K29>
2024-03-19 23:49:44 -0600

    delete border

--- hotels_worldwiden/Habitaciones.Designer.cs
+++ hotels_worldwiden/Habitaciones.Designer.cs
@@ -131,7 +131,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(916, 344);
+            this.tabPage2.Size = new System.Drawing.Size(1007, 500);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Ocupadas";
             // 
@@ -448,11 +448,11 @@
             // 
             // Habitaciones
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1119, 651);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Habitaciones";
             this.Text = "Habitaciones";
             this.Load += new System.EventHandler(this.Habitaciones_Load);
